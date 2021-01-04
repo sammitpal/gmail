c5b4a7b1300ef21ec679efb7e16571126d07cf16
sammitpal2000 <sammit.dp.pal2000@gmail.com>
2021-01-04 19:42:20 +0530

    sammit commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Header from './Header';
+import Sidebar from './Sidebar';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import EmailList from './EmailList';
+import Mail from './Mail'
+import Login from './Login';
+import {auth} from './firebase'
+import {useEffect} from 'react'
+import {useStateValue} from './StateProvider'
 function App() {
+  const [{user},dispatch] = useStateValue();
+  useEffect(()=>{
+    auth.onAuthStateChanged(authUser => {
+      console.log("USER ->", authUser);
+      if(authUser){
+        dispatch({
+          type: 'SET_USER',
+          user: authUser
+        })
+      }
+      else{
+        dispatch({
+          type: 'SET_USER',
+          user: null
+        })
+      }
+    })
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      {user?( <Router>
+    <div className="app">
+    <Header/>
+    <div className="app_body">
+    <Sidebar/>
+    <Switch>
+      <Route path="/mail/:mailid">
+        <Mail/>
+      </Route>
+      <Route path="/">
+        <EmailList/>
+      </Route>
+    </Switch>
     </div>
-  );
+  </div>
+  </Router>
+  ):(<Login/>)}
+    </>
+  )
 }
 
 export default App;
